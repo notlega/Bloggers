599a1a699ee60aea5714d9ada8571bc0ef651729
notlega <onghinjourn@gmail.com>
2022-10-28 13:04:13 +0800

    feat: :sparkles: initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>NextJS-Blog</title>
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
