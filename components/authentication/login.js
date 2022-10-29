4c59fd3752ee51ff212fb80ae907f042e63b60b3
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2022-10-30 01:32:32 +0800

    added required things for login

---
+++ components/authentication/login.js
@@ -0,0 +1,40 @@
+import {Auth, ThemeSupa} from '@supabase/auth-ui-react'
+import {useUser, useSupabaseClient} from '@supabase/auth-helpers-react'
+
+const providers = [
+  'apple',
+  'azure',
+  'bitbucket',
+  'discord',
+  'facebook',
+  'github',
+  'gitlab',
+  'google',
+  'keycloak',
+  'linkedin',
+  'messagebird',
+  'notion',
+  'slack',
+  'spotify',
+  'twitter',
+  'twitch',
+  'zoom',
+  'twilio',
+  'vonage'
+]
+const Login = () => {
+  const supabaseClient = useSupabaseClient()
+  return (
+    // <div style={{marginRight: 240, marginLeft: 240, marginTop: 60}}>
+    <Auth
+      redirectTo="http://localhost:3000/"
+      appearance={{theme: ThemeSupa}}
+      supabaseClient={supabaseClient}
+      providers={providers}
+      socialLayout="horizontal"
+    />
+    // </div>
+  )
+}
+
+export default Login
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,25 @@
 import '../styles/globals.css';
+import {useRouter} from "next/router";
+import {createBrowserSupabaseClient} from '@supabase/auth-helpers-nextjs'
+import {useState} from "react";
+import {SessionContextProvider} from "@supabase/auth-helpers-react/src/components/SessionContext";
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+function MyApp({Component, pageProps}) {
+  const router = useRouter()
+  // Create a new supabase browser client on every first render.
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+  const [theme, setTheme] = useState('dark');
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <div className={styles.container}>
+        <Component {...pageProps} />
+      </div>
+    </SessionContextProvider>
+  )
+}
 
 export default MyApp;
