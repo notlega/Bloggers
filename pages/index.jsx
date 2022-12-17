dd601ef33af9850dc80a047092e649a7fd76810e
notlega <onghinjourn@gmail.com>
2022-12-17 11:48:09 +0800

    feat: :sparkles: implements usequery for sample data

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,33 @@
 import { useState } from 'react';
+import { useQuery } from 'react-query';
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
 import BlogCard from '../components/BlogCard';
 
 const Home = () => {
+  const supabaseClient = useSupabaseClient();
+
+  const [blogs, setBlogs] = useState([]);
+
+  const {
+    isError: blogsError,
+    isLoading: blogsLoading,
+    status: blogsStatus,
+  } = useQuery(
+    'get_blogs',
+    () => supabaseClient.rpc('get_blogs', { blogs_limit: 10, blogs_offset }),
+    {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      onSuccess: (data) => {
+        setBlogs(data);
+      },
+    }
+  );
+
   return (
     <div className="flex flex-col justify-center items-center">
-      <BlogCard subHeader="Bing Chilling Section" title='bing chilling was first said by john cena back in 2019' />
-      <BlogCard title='bing chilling was first said by john cena back in 2019' />
+      {!blogsError && !blogsLoading && blogsStatus === 'success' && blogs.map((blog) => <BlogCard id={blog.id} title={blog.title} />)}
     </div>
   );
 };
