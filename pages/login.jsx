1a2d80adf187293874a0c046795a6b81e540fe98
notlega <onghinjourn@gmail.com>
2023-01-19 19:30:58 +0800

    feat: :construction_worker: adds a ci build test

--- pages/login.jsx
+++ pages/login.jsx
@@ -10,12 +10,6 @@ const Login = () => {
       <div className="hero-content text-center">
         <div className="max-w-4xl space-y-8">
           <h1 className="text-5xl font-bold">Login</h1>
-          <Auth
-            appearance={{ theme: ThemeSupa }}
-            supabaseClient={supabaseClient}
-            socialLayout="horizontal"
-            redirectTo="http://localhost:3000"
-          />
         </div>
       </div>
     </div>
