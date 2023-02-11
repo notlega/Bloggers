8fcba4038ee830160d4b8451dda918e2f8fb1b13
notlega <onghinjourn@gmail.com>
2023-02-12 04:57:41 +0800

    feat: :lipstick: updates the default layout
    adds a footer + changes in colour scheme

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useContext } from 'react';
 import { useSupabaseClient, useUser } from '@supabase/auth-helpers-react';
 import PropTypes from 'prop-types';
 import { IoSearch } from 'react-icons/io5';
 import { MdLogin, MdLogout } from 'react-icons/md';
 import { RxHamburgerMenu } from 'react-icons/rx';
 import Container from '../components/Container';
+import UserContext from '../contexts/UserContext';
 
 const propTypes = {
   children: PropTypes.node,
@@ -21,11 +22,7 @@ const DefaultLayout = ({ children }) => {
   const router = useRouter();
   const supabaseClient = useSupabaseClient();
   const user = useUser();
-
-  // TODO: get the profile picture from the database and not hardcode values
-  const [profilePicture, setProfilePicture] = useState(
-    'https://wzidvlwiavpggnktzbuk.supabase.co/storage/v1/object/public/avatar-image-bucket/default-profile-picture'
-  );
+  const { isLoading: isUserLoading, user: publicUser } = useContext(UserContext);
 
   const logOut = () => {
     const { error } = supabaseClient.auth.signOut();
@@ -40,16 +37,21 @@ const DefaultLayout = ({ children }) => {
   };
 
   return (
-    <div className="drawer drawer-end drawer-mobile">
-      <input type="checkbox" id="right-drawer" className="drawer-toggle" />
-      <div className="drawer-content">
-        <Container>
-          <nav className="navbar w-full sticky top-0 py-2.5 space-x-2 justify-between">
+    !isUserLoading && (
+      <div className="drawer drawer-end drawer-mobile">
+        <input type="checkbox" id="right-drawer" className="drawer-toggle" />
+        <div className="drawer-content">
+          <nav className="navbar w-full sticky top-0 py-2.5 z-20 bg-base-300 space-x-2 justify-between">
             <div className="flex-1">
               <Link href="/" className="btn btn-ghost text-xl normal-case">
                 Bloggers!
               </Link>
             </div>
+            {user && (
+              <Link href="/new-blog" className="btn">
+                + New Blog
+              </Link>
+            )}
             <div className="hidden md:block input-group w-auto">
               <input type="text" className="input input-bordered w-64" placeholder="Search..." />
               <button className="btn btn-square">
@@ -60,7 +62,7 @@ const DefaultLayout = ({ children }) => {
               <div className="hidden md:inline-block dropdown dropdown-end">
                 <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                   <div className="w-10 rounded-full">
-                    <img src={profilePicture} alt={profilePicture} />
+                    <img src={publicUser.avatar} alt={publicUser.username} />
                   </div>
                 </label>
                 <ul
@@ -68,7 +70,7 @@ const DefaultLayout = ({ children }) => {
                   className="mt-3 p-2 shadow-lg menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
                 >
                   <li>
-                    <Link href="/profile">Profile</Link>
+                    <Link href={`/profile/${user.id}`}>Profile</Link>
                   </li>
                   <li>
                     <button onClick={logOut}>Logout</button>
@@ -88,66 +90,115 @@ const DefaultLayout = ({ children }) => {
               <RxHamburgerMenu className="w-6 h-6" />
             </label>
           </nav>
-          {children}
-        </Container>
-      </div>
-      <div className="drawer-side md:hidden">
-        <label htmlFor="right-drawer" className="drawer-overlay" />
-        <ul className="menu p-4 w-80 bg-base-100 md:hidden">
-          <li>
-            <div className="input-group gap-0">
-              <input
-                type="text"
-                id="search-navbar"
-                className="input input-bordered"
-                placeholder="Search..."
-              />
-              <button className="btn btn-square">
-                <IoSearch className="w-fit h-fit" />
-              </button>
+          <Container>{children}</Container>
+          <footer className="p-10 pb-4 bg-neutral text-neutral-content space-y-10">
+            <div className='footer'>
+              <div>
+                <span className="footer-title">Services</span>
+                <Link href="/" className="link link-hover">
+                  Branding
+                </Link>
+                <Link href="/" className="link link-hover">
+                  Design
+                </Link>
+                <Link href="/" className="link link-hover">
+                  Marketing
+                </Link>
+                <Link href="/" className="link link-hover">
+                  Advertisement
+                </Link>
+              </div>
+              <div>
+                <span className="footer-title">Company</span>
+                <Link href="/" className="link link-hover">
+                  About us
+                </Link>
+                <Link href="/" className="link link-hover">
+                  Contact
+                </Link>
+                <Link href="/" className="link link-hover">
+                  Jobs
+                </Link>
+                <Link href="/" className="link link-hover">
+                  Press kit
+                </Link>
+              </div>
+              <div>
+                <span className="footer-title">Legal</span>
+                <Link href="/" className="link link-hover">
+                  Terms of use
+                </Link>
+                <Link href="/" className="link link-hover">
+                  Privacy policy
+                </Link>
+                <Link href="/" className="link link-hover">
+                  Cookie policy
+                </Link>
+              </div>
             </div>
-          </li>
-          {user ? (
-            <>
+            <div className="footer footer-center">
+              <p>Copyright © 2023 - All right reserved by notlega</p>
+            </div>
+          </footer>
+        </div>
+        <div className="drawer-side md:hidden">
+          <label htmlFor="right-drawer" className="drawer-overlay" />
+          <ul className="menu p-4 w-80 bg-base-100 md:hidden">
+            <li>
+              <div className="input-group gap-0">
+                <input
+                  type="text"
+                  id="search-navbar"
+                  className="input input-bordered"
+                  placeholder="Search..."
+                />
+                <button className="btn btn-square">
+                  <IoSearch className="w-fit h-fit" />
+                </button>
+              </div>
+            </li>
+            {user ? (
+              <>
+                <li>
+                  <Link
+                    href={`/profile/${user.id}`}
+                    className="inline-flex flex-wrap items-center text-center text-md"
+                  >
+                    <div className="object-cover">
+                      <img
+                        src={publicUser.avatar}
+                        alt={publicUser.username}
+                        className="mt-0.5 w-8 h-8 rounded-full"
+                      />
+                    </div>
+                    Profile
+                  </Link>
+                </li>
+                <li>
+                  <button
+                    className="inline-flex flex-wrap items-center text-center text-md"
+                    onClick={logOut}
+                  >
+                    <MdLogout className="mt-0.5 w-fit h-5/6" />
+                    Logout
+                  </button>
+                </li>
+              </>
+            ) : (
               <li>
                 <Link
-                  href="/profile"
+                  href="/login"
                   className="inline-flex flex-wrap items-center text-center text-md"
                 >
-                  <div className="object-cover">
-                    <img
-                      src={profilePicture}
-                      alt="temp_image"
-                      className="mt-0.5 w-8 h-8 rounded-full"
-                    />
-                  </div>
-                  Profile
+                  <MdLogin className="mt-0.5 w-fit h-5/6" />
+                  Login
                 </Link>
               </li>
-              <li>
-                <button
-                  className="inline-flex flex-wrap items-center text-center text-md"
-                  onClick={logOut}
-                >
-                  <MdLogout className="mt-0.5 w-fit h-5/6" />
-                  Logout
-                </button>
-              </li>
-            </>
-          ) : (
-            <li>
-              <Link
-                href="/login"
-                className="inline-flex flex-wrap items-center text-center text-md"
-              >
-                <MdLogin className="mt-0.5 w-fit h-5/6" />
-                Login
-              </Link>
-            </li>
-          )}
-        </ul>
+            )}
+          </ul>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
